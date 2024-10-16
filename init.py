55d519029892a6ae52619b7204c7295dbf9a8c1c
Martin Tomek <themolxyt@gmail.com>
2024-10-16 11:51:56 +0200

    Update init.py

--- init.py
+++ init.py
@@ -1,15 +1,5 @@
 
 import nuke
-import cryptotools
-import projectsetup
-import maskcheckergrade
-import maskcheckerpremult
-import sequenceloader
-import zdefocuschecker
-import LoadLightningRender
-import LightShuffler
-import ReduceNoiseBackdrop
-import NewDenoiseComp
 
 
 print("Snazim se co nejrychleji")
